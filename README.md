aca99dbb0ddc98ef203e2e7e303512f322711cf6
jean.barreto <jean.barreto@rappi.com>
2022-10-16 13:29:54 -0300

    add change to new way to download video

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ this project is an app with you can download youtube video
 1.- Should execute the application with yout OS
 2.- You should write the video url youtube
 3.- You should look the folder if exist the video downloaded and Done
+4.- You need to create one file with name config.json and add change this values
--- download_youtube_video.js
+++ download_youtube_video.js
@@ -1,17 +1,48 @@
+const _ = require('lodash');
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 const readline = require('readline');
+const config = require('./config.json')
 const { stdin: input, stdout: output } = require('process');
 
-const rl = readline.createInterface({ input, output });
+let minStart = 0;
+let minFinished = 0;
 
-rl.question('Url to download: ', (answer) => {
-  console.log(`the video is: ${answer}`);
-  let url = answer;
-  ytdl(url, { filter: format => format.container === 'mp4' })
+
+const rl = readline.createInterface({input, output});
+
+const buildListVideos = async (urls) => {
+  const urls_splited = urls.split('||');
+  console.log(`the videos are: ${urls_splited.length}`);
+  urls_splited.forEach((element, i) => {
+    ytdl(element, { filter: format => format.container === 'mp4', quality: 'highestaudio' })
+    .pipe(fs.createWriteStream(`video_${i}.mp4`))
+  });
+  console.log(`Creating video...`);
+}
+
+const buildSingleVideo = async (url) => {
+  console.log(`the video is: ${url}`);
+  ytdl(url, { filter: format => format.container === 'mp4', range: {start: minStart, end: minFinished} })
     .pipe(fs.createWriteStream('video.mp4'))
-  console.log(`Creando video...`);
+  console.log(`Creating video...`);
   rl.close();
+}
+
+rl.question('Url to download: ', async (answer) => {
+  rl.question('Do you have a specific min to start?', async (answerTwo) => {
+    rl.question('Do you have a specific min to finish?', async(answerThree) => {
+      const isEnableList = config.is_enable_list;
+      minStart = +answerTwo;
+      minFinished = +answerThree;
+      const isList = _.isArray(answer);
+      if(isEnableList && isList){
+        await buildListVideos(answer);
+      }else {
+        await buildSingleVideo(answer);
+      }
+    });
+  });
 });
 
 rl.on('close', () => {
